0327dd2933b669fd47c84f82aa779ba7b45fdf04
Binod Swain <binodkumarswain94@gmail.com>
2020-07-13 11:21:45 -0700

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,20 +1,14 @@
 import React, { PureComponent } from "react";
 // import PropTypes from "prop-types";
 
-import style from "./styles.scss";
+import "./styles.scss";
 
 class RootComponent extends PureComponent {
     static propTypes = {};
 
     render() {
-        return <div className={style["app"]}>Hello from react-lib-template</div>;
+        return <div className="app">Hello from react-lib-template</div>;
     }
 }
 
-let rootComponent;
-
-if (typeof window !== "undefined") {
-    rootComponent = RootComponent;
-}
-
-export default rootComponent;
+export default RootComponent;
