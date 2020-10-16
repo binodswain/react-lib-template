0499cec3eae3f7ead9eccd3e4676dc030e8dbfa9
Binod Swain <binodkumarswain94@gmail.com>
2020-10-16 22:24:21 +0530

    fix: update text and styles in package and example

--- src/index.js
+++ src/index.js
@@ -7,7 +7,11 @@ class RootComponent extends PureComponent {
     static propTypes = {};
 
     render() {
-        return <div className="app">Hello from react-lib-template</div>;
+        return (
+            <div className="app">
+                <h1>👋 Hello from react-lib-template</h1>
+            </div>
+        );
     }
 }
 
